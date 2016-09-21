76a1a8167cc808dabb79b27465bcfa2de205b2de
crownedpix <crownedpix@gmail.com>
2016-09-21 13:18:18 +0530

    Angular Integrated

--- app/app.js
+++ app/app.js
@@ -1,28 +1,6 @@
-angular.module("crowned", []), angular.module("crowned").config(function(a, b) {
-    a.state("login", {
-        url: "/login",
-        templateUrl: "app/components/login/login.html",
-        controller: "login"
-    }).state("main", {
-        url: "/main",
-        views: {
-            "": {
-                templateUrl: "app/components/main/main.html"
-            },
-            "head@main": {
-                templateUrl: "app/shared/head/head.html"
-            },
-            "submenu@main": {
-                templateUrl: "app/shared/menu/menu.html"
-            }
-        }
-    }).state("main.about", {
-        url: "/about",
-        templateUrl: "app/components/about/about.html",
-        controller: "about"
-    }).state("main.profile", {
-        url: "/profile",
-        templateUrl: "app/components/profile/profile.html",
-        controller: "profile"
-    }), b.otherwise("/login");
+angular.module("crowned", [ "ui.router" ]), angular.module("crowned").config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state("front", {
+        url: "/",
+        templateUrl: "views/banner/banner.html"
+    }), $urlRouterProvider.otherwise("/");
 });
