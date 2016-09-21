ddda3c7d931d1347077d18fbc839cb858596f5f0
crownedpix <crownedpix@gmail.com>
2016-09-22 01:46:28 +0530

    url fixed

--- app/app.js
+++ app/app.js
@@ -1,6 +1,22 @@
-angular.module("crowned", [ "ui.router" ]), angular.module("crowned").config(function($stateProvider, $urlRouterProvider) {
-    $stateProvider.state("front", {
-        url: "/",
+angular.module("crowned", [ "ui.router" ]), angular.module("crowned").config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider.state("main", {
+        url: "/main",
+        views: {
+            "": {
+                templateUrl: "views/main/main.html"
+            },
+            "head@main": {
+                templateUrl: "views/head/head.html"
+            },
+            "foot@main": {
+                templateUrl: "views/foot/foot.html"
+            }
+        }
+    }).state("main.front", {
+        url: "/front",
         templateUrl: "views/banner/banner.html"
-    }), $urlRouterProvider.otherwise("/");
+    }), $urlRouterProvider.otherwise("/main/front"), $locationProvider.html5Mode({
+        enabled: !0,
+        requireBase: !1
+    });
 });
