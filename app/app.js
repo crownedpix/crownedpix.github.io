03c56924095b9e538ade34cbb42c5ea2e505b78b
crownedpix <devecha.abid@gmail.com>
2021-09-05 21:54:54 +0530

    New design1

---
+++ app/app.js
@@ -0,0 +1,47 @@
+angular.module("crowned", [ "ui.router" ]), angular.module("crowned").config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state("main", {
+        url: "/main",
+        views: {
+            "": {
+                templateUrl: "views/main/main.html"
+            },
+            "head@main": {
+                templateUrl: "views/head/head.html"
+            },
+            "foot@main": {
+                templateUrl: "views/foot/foot.html"
+            }
+        }
+    }).state("main.front", {
+        url: "/front",
+        templateUrl: "views/banner/banner.html",
+        controller: "frontpageController"
+    }).state("main.about", {
+        url: "/about",
+        templateUrl: "views/about/about.html"
+    }), $urlRouterProvider.otherwise("/main/front");
+}), angular.module("crowned").controller("frontpageController", function($scope, $http, $rootScope) {
+    $scope.mailStatus = !0, $scope.status = "", $scope.sendMail = function(contact) {
+        $http({
+            method: "POST",
+            url: "https://api.sendgrid.com/api/mail.send.json",
+            data: $.param({
+                api_user: "crownedpix",
+                api_key: "5thDecember",
+                to: "devecha.abid@gmail.com",
+                toname: "Crownedpix Admin",
+                subject: "Crownedpix - Website Feedback Email",
+                html: $scope.contact.message + "<p>Phone number:" + $scope.contact.phone + "</p>",
+                from: $scope.contact.email
+            }),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        }).success(function(data, status) {
+            console.log(data), $scope.mailStatus = !1, $scope.status = "Mail sent Successfully", 
+            $scope.contact = [];
+        }).error(function(data, status) {
+            console.log("Error " + data);
+        });
+    };
+});
