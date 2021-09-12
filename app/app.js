022380292739ca718b78e574392a30c470b6ac1b
crownedpix <devecha.abid@gmail.com>
2021-09-12 07:40:00 +0530

    JSON

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
-angular.module("crowned", [ "ui.router" ]), angular.module("crowned").config(function($stateProvider, $urlRouterProvider) {
+var app = angular.module("crowned", [ "ui.router" ]); 
+app.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider.state("main", {
         url: "/main",
         views: {
@@ -20,28 +21,38 @@ angular.module("crowned", [ "ui.router" ]), angular.module("crowned").config(fun
         url: "/about",
         templateUrl: "views/about/about.html"
     }), $urlRouterProvider.otherwise("/main/front");
-}), angular.module("crowned").controller("frontpageController", function($scope, $http, $rootScope) {
-    $scope.mailStatus = !0, $scope.status = "", $scope.sendMail = function(contact) {
-        $http({
-            method: "POST",
-            url: "https://api.sendgrid.com/api/mail.send.json",
-            data: $.param({
-                api_user: "crownedpix",
-                api_key: "5thDecember",
-                to: "devecha.abid@gmail.com",
-                toname: "Crownedpix Admin",
-                subject: "Crownedpix - Website Feedback Email",
-                html: $scope.contact.message + "<p>Phone number:" + $scope.contact.phone + "</p>",
-                from: $scope.contact.email
-            }),
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        }).success(function(data, status) {
-            console.log(data), $scope.mailStatus = !1, $scope.status = "Mail sent Successfully", 
-            $scope.contact = [];
-        }).error(function(data, status) {
-            console.log("Error " + data);
-        });
-    };
+});
+app.controller("frontpageController", function($scope, $http, $rootScope) {
+    // $scope.mailStatus = !0, $scope.status = "", $scope.sendMail = function(contact) {
+    //     $http({
+    //         method: "POST",
+    //         url: "https://api.sendgrid.com/api/mail.send.json",
+    //         data: $.param({
+    //             api_user: "crownedpix",
+    //             api_key: "5thDecember",
+    //             to: "devecha.abid@gmail.com",
+    //             toname: "Crownedpix Admin",
+    //             subject: "Crownedpix - Website Feedback Email",
+    //             html: $scope.contact.message + "<p>Phone number:" + $scope.contact.phone + "</p>",
+    //             from: $scope.contact.email
+    //         }),
+    //         headers: {
+    //             "Content-Type": "application/x-www-form-urlencoded"
+    //         }
+    //     }).success(function(data, status) {
+    //         console.log(data), $scope.mailStatus = !1, $scope.status = "Mail sent Successfully", 
+    //         $scope.contact = [];
+    //     }).error(function(data, status) {
+    //         console.log("Error " + data);
+    //     });
+    // };
+    $scope.headData;
+    $scope.fetchData = function(){
+        $http.get('../json/head.json').success(function(data){
+            $scope.headData = data;
+        }).error(function(err){
+            console.log('file not found');
+        })
+    }
+    $scope.fetchData();
 });
